81cd76341007ad824dc9142f5cf146bac3d3f01b
Patrick Opie <opiepj@Plu.edu>
2015-07-02 16:16:00 -0700

    Util files for the depth first search implementation.

---
+++ app/utils/dataStructures/stackQueue.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var linkedList = require('./linkedList.js');
+
+var stackQueue = function (){
+  this.list = new linkedList();
+  this.length = 0;
+};
+
+stackQueue.prototype.push = function (data) {
+  this.list.add(data);
+  this.length++;
+};
+
+stackQueue.prototype.pop = function () {
+  if (this.isEmpty()) { throw new Error('The stack/queue is empty'); }
+
+  var results = this.peek();
+
+  this.list.remove(results);
+  this.length--;
+  return results;
+};
+
+stackQueue.prototype.isEmpty = function() {
+  return this.length === 0;
+};
+
+stackQueue.prototype.clear = function() {
+  this.list = new linkedList();
+  this.length = 0;
+};
+
+stackQueue.prototype.peek = function() {
+  return this.isEmpty() ? null : this.getNext();
+};
+
+module.exports = stackQueue;
